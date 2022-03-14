d70634236ae29115713ff6dd54aa409fa5bdbe05
Jonibek WA <jonee.av@gmail.com>
2022-03-14 18:12:30 +0500

    initial commit

---
+++ src/main/java/team/bahor/BoomAcademyApplication.java
@@ -0,0 +1,13 @@
+package team.bahor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoomAcademyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoomAcademyApplication.class, args);
+    }
+
+}
