e49addd8ce3b4714c673d80c5c495e89bbfaa618
abdumajid2001 <95360907+abdumajid2001@users.noreply.github.com>
2022-03-25 15:59:45 +0500

    exam and UserDetails

--- src/main/java/team/bahor/mappers/exam/ExamQuestionGenerationMapper.java
+++ src/main/java/team/bahor/mappers/exam/ExamQuestionGenerationMapper.java
@@ -1,5 +1,8 @@
 package team.bahor.mappers.exam;
 
+import org.mapstruct.Mapper;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.springframework.stereotype.Component;
 import team.bahor.dto.exam.examQuestionGeneration.ExamQuestionGenerationCreateDto;
 import team.bahor.dto.exam.examQuestionGeneration.ExamQuestionGenerationDto;
 import team.bahor.dto.exam.examQuestionGeneration.ExamQuestionGenerationUpdateDto;
@@ -7,6 +10,8 @@ import team.bahor.entity.exam.ExamQuestionGeneration;
 import team.bahor.mappers.base.AbstractMapper;
 import team.bahor.mappers.base.BaseGenericMapper;
 
+@Component
+@Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface ExamQuestionGenerationMapper extends AbstractMapper<
         ExamQuestionGeneration,
         ExamQuestionGenerationDto,
