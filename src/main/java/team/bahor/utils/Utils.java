dafd14e660cd9e3590f0399bb8f9bbb86b1a081d
abdumajid2001 <95360907+abdumajid2001@users.noreply.github.com>
2022-03-25 15:07:25 +0500

    exam and UserDetails

--- src/main/java/team/bahor/utils/Utils.java
+++ src/main/java/team/bahor/utils/Utils.java
@@ -1,4 +1,16 @@
 package team.bahor.utils;
 
+import org.springframework.security.core.context.SecurityContextHolder;
+import team.bahor.config.security.UserDetails;
+
 public class Utils {
+    public static String getSessionId(){
+        return ((UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getId();
+    }
+    public static boolean sessionHasRole(String role){
+        return ((UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getAuthorities().stream()
+                .filter(grantedAuthority -> {
+                    return grantedAuthority.getAuthority().equalsIgnoreCase("ROLE"+role);
+                }).count()==1;
+    }
 }
