bfcf0a6686ce4a14230296c57761cc1d721fccf4
abdumajid2001 <95360907+abdumajid2001@users.noreply.github.com>
2022-03-26 18:26:05 +0500

    26:03:2022 abdumajid change endi id olsa boladi

--- src/main/java/team/bahor/controller/TestController.java
+++ src/main/java/team/bahor/controller/TestController.java
@@ -1,6 +1,8 @@
 package team.bahor.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import team.bahor.dto.responce.DataDto;
