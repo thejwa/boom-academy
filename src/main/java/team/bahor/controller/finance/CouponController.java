8ce69aae05eec382f0a162c5be5ebcfdef5bcec6
dilshod <dilshooodbek99akhmedov76@gmail.com>
2022-04-01 14:18:21 +0500

    tag CRUD! and coupon modern

--- src/main/java/team/bahor/controller/finance/CouponController.java
+++ src/main/java/team/bahor/controller/finance/CouponController.java
@@ -1,18 +1,21 @@
 package team.bahor.controller.finance;
 
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import team.bahor.controller.AbstractController;
 import team.bahor.dto.finance.CouponCreateDto;
 import team.bahor.dto.responce.DataDto;
-import team.bahor.services.finance.CouponService;
+import team.bahor.services.finance.CouponServiceImp;
 
 @RestController
 @RequestMapping("/coupon/")
-public class CouponController extends AbstractController<CouponService> {
-    public CouponController(CouponService service) {
+public class CouponController extends AbstractController<CouponServiceImp> {
+
+    public CouponController(CouponServiceImp service) {
         super(service);
     }
 
--- src/main/java/team/bahor/services/finance/CouponService.java
+++ src/main/java/team/bahor/services/finance/CouponService.java
@@ -1,52 +1,13 @@
 package team.bahor.services.finance;
 
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Service;
 import team.bahor.dto.finance.CouponCreateDto;
 import team.bahor.dto.finance.CouponDto;
 import team.bahor.dto.finance.CouponUpdateDto;
-import team.bahor.mappers.finance.CouponMapper;
-import team.bahor.repositories.finance.CouponRepository;
-import team.bahor.services.base.AbstractService;
 import team.bahor.services.base.GenericCrudService;
-import team.bahor.validators.finance.CouponValidator;
 
-import java.util.List;
-
-@Service
-
-public class CouponService extends AbstractService<CouponRepository, CouponMapper, CouponValidator>
-        implements GenericCrudService<
+public interface CouponService extends GenericCrudService<
         CouponDto,
         CouponCreateDto,
         CouponUpdateDto,
         String> {
-    protected CouponService(@Qualifier("couponMapperImpl") CouponMapper mapper, CouponValidator validator, CouponRepository repository) {
-        super(mapper, validator, repository);
-    }
-
-    @Override
-    public String create(CouponCreateDto createDto) {
-        return null;
-    }
-
-    @Override
-    public void delete(String id) {
-
-    }
-
-    @Override
-    public void update(CouponUpdateDto updateDto) {
-
-    }
-
-    @Override
-    public CouponDto get(String id) {
-        return null;
-    }
-
-    @Override
-    public List<CouponDto> getAll() {
-        return null;
-    }
 }
