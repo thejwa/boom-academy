6c5d23a5b409b824804a622b425679b7a3f8c197
dilshod <dilshooodbek99akhmedov76@gmail.com>
2022-04-07 23:01:10 +0500

    Course Purchase

--- src/main/java/team/bahor/dto/course/purchase/CoursePurchaseUpdateDto.java
+++ src/main/java/team/bahor/dto/course/purchase/CoursePurchaseUpdateDto.java
@@ -4,7 +4,17 @@ import lombok.Getter;
 import lombok.Setter;
 import team.bahor.dto.GenericDto;
 
+import javax.validation.constraints.NotNull;
+
 @Getter
 @Setter
 public class CoursePurchaseUpdateDto extends GenericDto {
+    @NotNull(message = "courseId is null")
+    private String courseId;
+
+    @NotNull(message = "userId is null")
+    private String userId;
+
+    private String couponCode;
+
 }
